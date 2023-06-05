9adea579c7a8d77bf75a3053e85b7b39ed564ceb
MML <76636013+MML4379@users.noreply.github.com>
2023-06-05 12:45:05 -0400

    adds emoji trail

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const cursor = document.getElementById('cursor');
 const cursorX = document.getElementById('cursorX');
 const cursorY = document.getElementById('cursorY');
+const emojis = document.getElementById('emojis');
 
 window.addEventListener('mousemove', (e) => {
     const { clientX, clientY } = e;
@@ -10,14 +11,21 @@ window.addEventListener('mousemove', (e) => {
     }, {fill: "forwards"});
     cursorX.textContent = clientX;
     cursorY.textContent = clientY;
-    
-    window.addEventListener('keydown', (e) => {
-    if (e.keyCode === "space") {
-        clientX = 690;
-        clientY = 690;
-        cursor.style.left = 690;
-        cursor.style.top = 690;    
-    }
-})
+    emojis.textContent = makeid(15);
+    emojis.animate({
+        left: `${clientX}px`,
+        top: `${clientY}px`
+    }, {duration=1200, fill="forwards"});
 });
 
+function makeid(length) {
+    let result = '';
+    const characters = '😁😀😂🤣😃😄😅😆😉😊😋😎😍😘🥰😗😙😚☺🙂🤩🤗🤔🤨😐😑😶🙄😣😏😥😮';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      counter += 1;
+    }
+    return result;
+}
